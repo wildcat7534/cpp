b97d21645749307e2457a2a52c70d089bade6837
Prof Chaos <Brakebein@gmail.com>
2025-11-05 21:05:40 +0100

    Lapinou est passé par ici

--- cpp_01/ex05/main.cpp
+++ cpp_01/ex05/main.cpp
@@ -1,10 +1,24 @@
 #include "Harl.hpp"
 
-int main() {
+int main(int ac, char **av) {
     Harl harl;
-    harl.complain("DEBUG");
-    harl.complain("INFO");
-    harl.complain("WARNING");
-    harl.complain("ERROR");
-    harl.complain("BOUH");
+	if (ac == 1)
+	{
+        harl.complain("DEBUG");
+        harl.complain("INFO");
+        harl.complain("WARNING");
+        harl.complain("ERROR");
+        harl.complain("BOUH");
+	}
+    else if (ac == 2)
+    {
+        harl.complain(av[1]);
+    }
+    else
+    {
+        std::cerr << "Usage: " << av[0] << " [level]" << std::endl;
+        std::cerr << "Available levels: DEBUG, INFO, WARNING, ERROR" << std::endl;
+        return 1;
+    }
+    return 0;
 }
