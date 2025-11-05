a5d7bab091081e46ba4272233d8f479b11210c01
Prof Chaos <Brakebein@gmail.com>
2025-11-05 20:28:18 +0100

    Lapinou est passé par ici

--- cpp_01/ex02/main.cpp
+++ cpp_01/ex02/main.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 
 int main(){
+
+    std::cout << std::endl << "----- Testing pointers and references -----" << std::endl << std::endl;
+
     std::string str = "HI THIS IS BRAIN";
 
     std::string *stringPTR = &str;
     std::string &stringREF = str;
 
+    std::cout << std::endl << "----- Addresses -----" << std::endl << std::endl;
     std::cout << &str << std::endl;
     std::cout << stringPTR << std::endl;
     std::cout << &stringREF << std::endl;
 
+    std::cout << std::endl << "----- Values -----" << std::endl << std::endl;
+    
     std::cout << str << std::endl;
     std::cout << *stringPTR << std::endl;
     std::cout << stringREF << std::endl;
 
+    std::cout << std::endl << "----- End of tests (garbage collection) -----" << std::endl << std::endl;
+    return 0;
 }
