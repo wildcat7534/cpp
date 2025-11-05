120ed83053fb61b5898ea2f722d986f77e9fe6eb
Prof Chaos <Brakebein@gmail.com>
2025-11-05 21:16:59 +0100

    Lapinou est passé par ici

--- cpp_02/ex00/main.cpp
+++ cpp_02/ex00/main.cpp
@@ -2,18 +2,29 @@
 
 int main(){
 
+	std::cout << std::endl << "----- Testing Fixed class -----" << std::endl << std::endl;
+
 	Fixed a;
 	//std::cout << "a : " << &a << std::endl << std::endl;
+
+	std::cout << std::endl << "----- Testing copy constructor -----" << std::endl << std::endl;
 
 	Fixed b(a);
 	//std::cout << "b : " << &b << std::endl << std::endl;
 
 	//Fixed a;
 	//Fixed b( a );
+
+	std::cout << std::endl << "----- Testing assignment operator -----" << std::endl << std::endl;
 	Fixed c;
 	c = b;
+
+	std::cout << std::endl << "----- Getting raw bits -----" << std::endl << std::endl;
 
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
+
+	std::cout << std::endl << "----- End of tests (garbage collection) -----" << std::endl << std::endl;
+	return 0;
 }
