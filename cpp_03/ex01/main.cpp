688f8df4fcb381756807e79a6fe559cfe63b69fb
Prof Chaos <Brakebein@gmail.com>
2025-11-05 21:52:23 +0100

    Lapinou est passé par ici

--- cpp_03/ex01/main.cpp
+++ cpp_03/ex01/main.cpp
@@ -1,6 +1,9 @@
 #include "ScavTrap.hpp"
 
 int main (){
+
+	std::cout << std::endl << "----- Testing ScavTrap class -----" << std::endl << std::endl;
+
 	ScavTrap robot0;
 	ScavTrap robot1("M. Clapi");
 	ScavTrap robot2("Mme Rainette");
@@ -25,11 +28,20 @@ int main (){
 
 	robot2.beRepaired(0);
 
-	ScavTrap robot3(robot2);
-	ScavTrap robot4 = robot1;
+	std::cout << std::endl << "----- Testing ScavTrap copy constructor and assignment operator -----" << std::endl << std::endl;
 
-	robot3.beRepaired(5);
-	robot1.attack("");
+	{
+		ScavTrap robot3(robot2);
+		ScavTrap robot4 = robot1;
+	
+		robot3.beRepaired(5);
+		robot1.attack("");
+		std::cout << "robot3 name : " << robot3.getName() << std::endl;
+		std::cout << "robot4 name : " << robot4.getName() << std::endl;
+	}
+	std::cout << "robot2 name : " << robot2.getName() << std::endl;
+	std::cout << "robot1 name : " << robot1.getName() << std::endl;	
 
+	std::cout << std::endl << "----- End of tests (garbage collection) -----" << std::endl << std::endl;
 	return 0;
 }
