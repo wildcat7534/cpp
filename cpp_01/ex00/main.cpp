9af2d9566c8d746fb8e2553f2e2e39d95e68230a
Prof Chaos <Brakebein@gmail.com>
2025-11-05 20:04:17 +0100

    Lapinou est passé par ici

--- cpp_01/ex00/main.cpp
+++ cpp_01/ex00/main.cpp
@@ -1,14 +1,30 @@
 #include "tools.hpp"
 
 int main(){
-    Zombie *ariane;
-    ariane = newZombie("Ariiiiane");
-    delete ariane;
+
+    std::cout << std::endl << "----- Testing newZombie and randomChump -----" << std::endl << std::endl;
+
+    Zombie *Ariane;
+    Ariane = newZombie("Ariiiiane");
+    delete Ariane;
     randomChump("Clemennnnnt");
 
     Zombie Kamila("Kamilaaa");
     Kamila.announce();
-    Zombie lapinou("Lapinouuuuu");
-    lapinou.announce();
+    Zombie Lapinou("Lapinouuuuu");
+    Lapinou.announce();
+
+    std::cout << std::endl << "----- Testing copy constructor and assignment operator -----" << std::endl << std::endl;
+    {
+        Zombie zombcopy = Kamila;
+        zombcopy.announce();
+        Zombie zombassign("Temporaire");
+        zombassign = Lapinou;
+        zombassign.announce();
+    }
+    Kamila.announce();
+    Lapinou.announce();
+
+    std::cout << std::endl << "----- End of tests (garbage collection) -----" << std::endl << std::endl;
     return 0;
 }
