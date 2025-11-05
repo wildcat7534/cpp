fb2f71edfd4fdf0641e5097b4ee6e2948ff482fc
Prof Chaos <Brakebein@gmail.com>
2025-11-05 20:21:12 +0100

    Lapinou est passé par ici et c'est pas beau à voir !

--- cpp_01/ex01/main.cpp
+++ cpp_01/ex01/main.cpp
@@ -2,8 +2,24 @@
 #include <iostream>
 
 int main(){
+
+    std::cout << std::endl << "----- Testing zombieHorde -----" << std::endl << std::endl;
+
     Zombie *horde;
     horde = zombieHorde(3, "laHorde");
-    if (horde == NULL)
-        std::cout << "Pointer is null ! GG" << std::endl;
+
+    if (!horde)
+        std::cerr << "Failed to create the horde of zombies." << std::endl;
+
+    std::cout << std::endl << "----- Announcing the horde -----" << std::endl << std::endl;
+    for (int i = 0; i < 3; ++i) {
+		horde[i].announce();
+	}
+
+    std::cout << std::endl << "----- Deleting the horde -----" << std::endl << std::endl;
+
+	delete[] horde;
+
+    std::cout << std::endl << "----- End of tests (garbage collection) -----" << std::endl << std::endl;
+    return 0;
 }
